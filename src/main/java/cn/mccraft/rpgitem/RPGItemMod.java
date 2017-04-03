135d75fc4caf1c960fab880fe081165c5bf9c981
Mouse <937279156@qq.com>
2017-04-03 19:02:21 +0800

    Init

---
+++ src/main/java/cn/mccraft/rpgitem/RPGItemMod.java
@@ -0,0 +1,10 @@
+package cn.mccraft.rpgitem;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = RPGItemMod.MODID,name = RPGItemMod.NAME,version = RPGItemMod.VERSION)
+public class RPGItemMod {
+    public static final String MODID = "rpgitemmod";
+    public static final String NAME = "RPGItemMod";
+    public static final String VERSION = "1.0.0";
+}
