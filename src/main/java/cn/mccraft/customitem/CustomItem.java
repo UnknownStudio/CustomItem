278896cbe8df944bb268780c41ff097ba9785b0b
Mouse <937279156@qq.com>
2017-04-03 19:27:55 +0800

    rename

---
+++ src/main/java/cn/mccraft/customitem/CustomItem.java
@@ -0,0 +1,10 @@
+package cn.mccraft.customitem;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CustomItem.MODID,name = CustomItem.NAME,version = CustomItem.VERSION)
+public class CustomItem {
+    public static final String MODID = "customitem";
+    public static final String NAME = "CustomItem";
+    public static final String VERSION = "1.0.0";
+}
